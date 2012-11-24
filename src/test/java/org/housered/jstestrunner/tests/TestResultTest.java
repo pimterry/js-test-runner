4476b446619024e5880fad2b3c5f192509aca07b
pimterry <pimterry@gmail.com>
2012-11-24 17:48:02 +0000

    Added proper tests for hashcode & equals of TestResults

--- src/test/java/org/housered/jstestrunner/tests/TestResultTest.java
+++ src/test/java/org/housered/jstestrunner/tests/TestResultTest.java
@@ -1,8 +1,11 @@
 package org.housered.jstestrunner.tests;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.housered.jstestrunner.tests.TestResult.TestCaseResult;
@@ -55,6 +58,37 @@ public class TestResultTest
         assertEquals(testCaseName2, result2.getTestName());
         assertEquals(testCaseResult2, result2.wasSuccess());
         assertEquals(testDurationMillis2, result2.getTestDurationMillis());
+    }
+    
+    @Test
+    public void emptyTestResultsShouldBeEqual() {
+        TestResult testResult1 = new TestResult(0, 0, 0, 0, 10, "empty test", Collections.<TestCaseResult> emptyList());
+        TestResult testResult2 = new TestResult(0, 0, 0, 0, 10, "empty test", Collections.<TestCaseResult> emptyList());
+        
+        assertEquals(testResult1, testResult2);
+        assertEquals(testResult1.hashCode(), testResult2.hashCode());
+    }
+    
+    @Test
+    public void sameTestResultsShouldBeEqualWithEqualHashCodes() {
+        TestResult testResult1 = new TestResult(2, 0, 1, 0, 10, "a test", Arrays.asList(
+                                                new TestCaseResult("testClass1", "test-name", true, 11),
+                                                new TestCaseResult("another class", "another test", false, 40)));
+        TestResult testResult2 = new TestResult(2, 0, 1, 0, 10, "a test", Arrays.asList(
+                                                new TestCaseResult("testClass1", "test-name", true, 11),
+                                                new TestCaseResult("another class", "another test", false, 40)));
+        
+        assertThat(testResult1, is(testResult2));
+        assertThat(testResult1.hashCode(), is(testResult2.hashCode()));
+    }
+    
+    @Test
+    public void differentTestResultsShouldBeInequalWithDifferentHashCodes() {
+        TestResult testResult1 = new TestResult(2, 0, 0, 0, 10, "a test", Collections.<TestCaseResult> emptyList());
+        TestResult testResult2 = new TestResult(3, 2, 0, 0, 10, "another test", Collections.<TestCaseResult> emptyList());
+        
+        assertThat(testResult1, is(not(testResult2)));
+        assertThat(testResult1.hashCode(), is(not(testResult2.hashCode())));
     }
     
 }
